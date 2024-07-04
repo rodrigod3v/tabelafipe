ece54d0ebdf3f22dc5339dacc8d61d8135484a17
Rodrigo Orlando <115408313+rodrigod3v@users.noreply.github.com>
2024-07-04 16:38:11 -0300

    Add files via upload

---
+++ src/test/java/br/com/alura/tabelafipe/TabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.tabelafipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
